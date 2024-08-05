768a06dacbeb838f4173178a04e337814b7c419e
filipefdiniz <filipefdiniz30@gmail.com>
2024-08-05 10:17:09 -0300

    uploading html with links

--- script.js
+++ script.js
@@ -14,21 +14,20 @@ var tempoLinks = 50000; // tempo em milissegundos para exibir cada link
 var indiceImagens = 0; // índice atual da imagem
 var indiceLinks = 0; // índice atual do link
 
-
 function mostrarImagem() {
     document.getElementById("minha-imagem").style.display = "block";
     document.getElementById("link-iframe").style.display = "none";
-   
+
     document.getElementById("minha-imagem").src = imagens[indiceImagens][0];
 
     timeOut = imagens[indiceImagens][1];
-    
+
     indiceImagens = (indiceImagens + 1) % imagens.length;
-    
+
     if (indiceImagens == 0) {
         //setTimeout(mostrarLink, tempoImagens);
         setTimeout(mostrarImagem, timeOut);
-        
+
     } else {
         setTimeout(mostrarImagem, timeOut);
     }
@@ -42,7 +41,7 @@ function mostrarLink() {
 
     if (indiceLinks == 0) {
         setTimeout(mostrarImagem, tempoLinks);
-        
+
     } else {
         setTimeout(mostrarLink, tempoLinks);
     }
@@ -51,65 +50,3 @@ function mostrarLink() {
 window.onload = function () {
     mostrarImagem();
 };
-
-// const tempoImagem = 10000;
-// const tempoIframe = 10000;
-// const tempoFade = 1000;
-// const tempoImagensExtras = 4000;
-
-// let indiceIframe = 0;
-// let indiceImagemExtra = 0;
-
-// function mostrarImagem() {
-//   const imagem = document.getElementById("minha-imagem");
-//   imagem.classList.add("show");
-//   setTimeout(() => {
-//     imagem.classList.remove("show");
-//     mostrarRelatorio();
-//   }, tempoImagem - tempoFade);
-// }
-
-// function mostrarRelatorio() {
-//   const iframesRelatorios = [
-//     document.getElementById("iframeGestaoAVista"),
-//     document.getElementById("iframeHabilidades"),
-//   ];
-
-//   iframesRelatorios.forEach((iframe) => iframe.classList.remove("show"));
-//   iframesRelatorios[indiceIframe].classList.add("show");
-
-//   setTimeout(() => {
-//     iframesRelatorios[indiceIframe].classList.remove("show");
-//     indiceIframe = (indiceIframe + 1) % iframesRelatorios.length;
-//     if (indiceIframe === 0) {
-//       if (document.getElementById("imagemExtra1")) {
-//         mostrarImagensExtras();
-//       } else {
-//         mostrarImagem();
-//       }
-//     } else {
-//       mostrarRelatorio();
-//     }
-//   }, tempoIframe - tempoFade);
-// }
-
-// function mostrarImagensExtras() {
-//   const imagensExtras = [document.getElementById("imagemExtra1")];
-
-//   imagensExtras.forEach((image) => image.classList.remove("show"));
-//   imagensExtras[indiceImagemExtra].classList.add("show");
-
-//   setTimeout(() => {
-//     imagensExtras[indiceImagemExtra].classList.remove("show");
-//     indiceImagemExtra = (indiceImagemExtra + 1) % imagensExtras.length;
-//     if (indiceImagemExtra === 0) {
-//       mostrarImagem();
-//     } else {
-//       mostrarImagensExtras();
-//     }
-//   }, tempoIframe - tempoFade);
-// }
-
-// window.onload = function () {
-//   mostrarImagem();
-// };
